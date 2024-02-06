16e4087fbb9e6a55d6c1fa0584eb4671e4e875e0
juan torres <juanonfleekmedia@gmail.com>
2024-02-06 14:47:32 -0500

    styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,28 +7,32 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
+        <?php
+            $version = '1993.0.1';
+        ?>
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
         <!-- vite(['resources/css/app.css', 'resources/js/app.js']) -->
-        <link href="css/app.css" rel="stylesheet">
+        <link href="{{asset('css/app.css')}}?v=<?php echo $version ?>" rel="stylesheet">
 
         <!-- Icon Font Stylesheet -->
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
         <!-- Libraries Stylesheet -->
-        <link href="lib/animate/animate.min.css" rel="stylesheet">
-        <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+        <link href="{{asset('lib/animate/animate.min.css')}}?v=<?php echo $version ?>" rel="stylesheet">
+        <link href="{{asset('lib/owlcarousel/assets/owl.carousel.min.css')}}?v=<?php echo $version ?>" rel="stylesheet">
 
 
         <!-- Customized Bootstrap Stylesheet -->
-        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="{{asset('css/bootstrap.min.css')}}?v=<?php echo $version ?>" rel="stylesheet">
 
         <!-- Template Stylesheet -->
-        <link href="css/style.css" rel="stylesheet">
+        <link href="{{asset('css/style.css')}}?v=<?php echo $version ?>" rel="stylesheet">
 
         
 
@@ -60,15 +64,15 @@
 
         @livewireScripts
 
-        <script src="js/app.js"></script>
+        <script src="{{asset('js/app.js')}}?v=<?php echo $version ?>"></script>
         <!-- JavaScript Libraries -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-        <script src="lib/easing/easing.min.js"></script>
-        <script src="lib/waypoints/waypoints.min.js"></script>
-        <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+        <script src="{{asset('lib/easing/easing.min.js')}}?v=<?php echo $version ?>"></script>
+        <script src="{{asset('lib/waypoints/waypoints.min.js')}}?v=<?php echo $version ?>"></script>
+        <script src="{{asset('lib/owlcarousel/owl.carousel.min.js')}}?v=<?php echo $version ?>"></script>
 
         <!-- Template Javascript -->
-        <script src="js/main.js"></script>
+        <script src="{{asset('js/main.js')}}?v=<?php echo $version ?>"></script>
     </body>
 </html>
