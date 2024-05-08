d2214c3428d1ecb8cc6bf2d9d079ccc1cb5016b6
juan torres <juanonfleekmedia@gmail.com>
2024-05-08 10:53:20 -0500

    version css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <?php
-            $version = '1993.0.8';
+            $version = '1993.0.9';
         ?>
 
         <!-- Fonts -->
