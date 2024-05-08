b1925496732b7d538ed0c337871801002f2297ff
juan torres <juanonfleekmedia@gmail.com>
2024-05-08 15:56:56 -0500

    cambios

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <?php
-            $version = '1993.1.0';
+            $version = '1993.1.1';
         ?>
 
         <!-- Fonts -->
@@ -78,7 +78,7 @@
 
         @livewireScripts
 
-        <!-- <script src="{{asset('js/app.js')}}?v=<?php echo $version ?>"></script> -->
+        <script src="{{asset('js/app.js')}}?v=<?php echo $version ?>"></script>
         <!-- JavaScript Libraries -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
